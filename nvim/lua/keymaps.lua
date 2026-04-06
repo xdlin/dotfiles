42139abafdd4555e89da750f41d5000ff20014cc
linxiangdong <linxiangdong@bytedance.com>
2026-04-06 11:21:30 -0700

    change keymaps
    Signed-off-by: linxiangdong <linxiangdong@bytedance.com>

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,11 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "open quickfix window" })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "open float window" })
 
+-- Diagnostic
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = "open quickfix window" })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = "open float window" })
+
+
+-- Search
 local pick = require("mini.pick")
 local extra = require("mini.extra")
 vim.keymap.set("n", "<C-p>", pick.builtin.files, { desc = "search files" })
@@ -19,9 +23,12 @@ vim.keymap.set("n", "<leader>sw", function()
   pick.builtin.grep({ pattern = vim.fn.expand("<cword>") })
 end, { desc = "search current word" })
 
-vim.keymap.set('n', "gr", function()
+vim.keymap.set('n', "sr", function()
   extra.pickers.lsp({ scope = 'references' })
 end)
+
+
+-- Toggle
 vim.keymap.set("n", "<leader>tt", function()
   local mf = require("mini.files")
   if mf.get_explorer_state() then
@@ -30,6 +37,8 @@ vim.keymap.set("n", "<leader>tt", function()
     mf.open(vim.api.nvim_buf_get_name(0), true)
   end
 end, { desc = "Toggle explorer" })
+
+vim.keymap.set("n", "<leader>ti", '<cmd>vertical term<CR>', { desc = "Terminal (vertical)" })
 
 -- diagnostic keymaps
 local diagnostic_goto = function(next, severity)
